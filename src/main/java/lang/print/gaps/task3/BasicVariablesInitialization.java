3df5d1ccae7805345460b263dedcb3797bdcee65
admin <nbisametova@mail.ru>
2022-08-13 00:54:24 +0600

    the second try

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,7 +2,9 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        int first, second, third;
+        int first;
+        int second;
+        int third;
         first = 1;
         second = 10;
         third = 100;
